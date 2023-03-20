fd072f12382e709b7294bf7b9fab4bf2de928a4a
bsze2hunist <bsze2hunist@SZT1-8-HP>
2023-03-20 18:09:59 +0100

    myCat Say...

--- Bevezeto_project/Bevezeto_project/Program.cs
+++ Bevezeto_project/Bevezeto_project/Program.cs
@@ -1,12 +1,27 @@
-﻿
-    /// <summary>
-    /// O1.01 Cat(macska) osztály
-    /// Az osztály egy kiterjesztett adat típus. Az osztály egy program kód sablon, amelyben 
-    /// állapotokat (adatokat) és viselkedési mintákat, műveleteket (metódusokat) lehet definiálni.
-    /// Az osztályok neveit nagybetűvel (PascalCase) írjuk.
-    /// Használjunk főneveket az osztály nevének megadásakor.
-    /// </summary>
-    public class Cat
+﻿// O1.05 OOP programozás
+// Az OOP programozás során a programokat objektumokból építjük fel.
+//  - Minden objektumnak megvan a jól meghatározott feladata.
+//  - Az objektumok képesek egymással kommunikálni.
+//  - OOP programozás során a programozást közelítjük az emberi gondolkodáshoz.
+
+// O1.06 Példányosítás
+// Cat osztály példányosítása (create instance of class).
+//  - A myCat objektum a Cat osztály egy példánya.
+//  - A lokális változókat írásakor a camelCaseing jelölést használunk.
+Cat myCat = new Cat();
+// O1.07 Példányosított objektum viselkedik (beszél)
+// A myCat objektum képes "beszélni". Az eredményt most a konzolon olvashatjuk.            
+myCat.Say();
+
+
+/// <summary>
+/// O1.01 Cat(macska) osztály
+/// Az osztály egy kiterjesztett adat típus. Az osztály egy program kód sablon, amelyben 
+/// állapotokat (adatokat) és viselkedési mintákat, műveleteket (metódusokat) lehet definiálni.
+/// Az osztályok neveit nagybetűvel (PascalCase) írjuk.
+/// Használjunk főneveket az osztály nevének megadásakor.
+/// </summary>
+public class Cat
 {
     /// <summary>
     /// O1.02 Cat osztály metódusa
@@ -40,15 +55,7 @@ public class Dog
 
 }
 
-            // O1.05 OOP programozás
-            // Az OOP programozás során a programokat objektumokból építjük fel.
-            //  - Minden objektumnak megvan a jól meghatározott feladata.
-            //  - Az objektumok képesek egymással kommunikálni.
-            //  - OOP programozás során a programozást közelítjük az emberi gondolkodáshoz.
 
-// O1.06 Példányosítás
-// Cat osztály példányosítása (create instance of class).
-//  - A myCat objektum a Cat osztály egy példánya.
-//  - A lokális változókat írásakor a camelCaseing jelölést használunk.
-            Cat myCat = new Cat();
+
+
 
